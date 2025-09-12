33f3686a196f00a06cf2aaac45eaa4008141fa9f
Jeriah Oliverasfair <joliverasfair@brooklynsteamcenter.org>
2025-09-12 10:52:14 -0400

    pp

---
+++ 02_formatting.py
@@ -0,0 +1,30 @@
+# I made a mango password so it can right password 
+password = "mangos123"
+
+
+u_input = input("put your password: ")
+
+
+if u_input.upper() == password.upper():
+    print("its your password!.")
+else:
+    print("This ain't yo account!.")
+    
+
+#Task 2 
+b_input = input("Enter now!: ")
+
+if b_input == "":
+    print("nope")
+else:
+    print("yup")
+
+
+
+
+
+
+
+
+
+
