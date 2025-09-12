974b7f962d6a2f511a5dd3ac37ac9332386cb49e
Jeriah Oliverasfair <joliverasfair@brooklynsteamcenter.org>
2025-09-12 09:41:22 -0400

    pp

--- 01_data_types.py
+++ 01_data_types.py
@@ -1,8 +1,78 @@
+
+print("------task1------") # labeling task 
+print("")
+print("")
+
+
 x = (15.0) #i made a float 
 
 print(x)# i printed said float 
 
 print(int(x))# i converted the float into an integer and printed it 
+
+print("------task1------") # labeling task 
+print("")
+print("")
+
+
+print("------task2------") # labeling task 
+print("")
+print("")
+
+input1 = input("give me a number plss:") # setting the input as a variable
+
+number1 = int(input1)# converting that inputs variable into an integer 
+
+if number1 > 0: # made an if, elif , and else statement to show if the integer input is positive, negative or 0
+    print("your number is positive")
+elif number1 < 0:
+    print("your number is negative")
+else :print("your number is 0")
+
+print("------task2------") # labeling task 
+print("")
+print("")
+print("")
+
+print("------task3------") # labeling task 
+print("")
+print("")
+
+input_a = input("give me a float:") # made a variable for an input
+
+num_1 = float(input_a) #converted that variable into a float
+
+print(type(input_a))
+
+input_b = input("give me a integer:") # made a variable for an input
+
+num_2 = int(input_b) #converted that variable into a integer
+
+print(type(input_b))
+
+print(num_1 + num_2) # adding the variables that was typed 
+
+print("------task3------") # labeling task 
+print("")
+print("")
+
+
+
+print("------task4------") # labeling task 
+print("")
+print("")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
