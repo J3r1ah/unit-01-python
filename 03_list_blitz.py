fcde15405189ee20ca6f8c7f44d2db75ce0ac01e
Jeriah Oliverasfair <joliverasfair@brooklynsteamcenter.org>
2025-09-15 10:50:20 -0400

    pp

---
+++ 03_list_blitz.py
@@ -0,0 +1,87 @@
+"""
+Task 1: Create a list
+Create a list with 4 elements and print it.
+"""
+
+list_mine = ["Daddy", "sunshine", 223, False, False]#made list
+
+print(list_mine)#printed list
+
+"""
+Task 2: Add Element to a List
+Add an element to the end of the list. Print the updated 
+list.
+"""
+
+list_mine.append("Mommy")#added mommy to my list
+
+print(list_mine)
+
+"""
+Task 3: Remove Element from a List
+Remove a specific element from the list. Print the 
+updated list.
+"""
+
+del list_mine[0] # i deleted daddy
+
+print(list_mine)
+
+"""
+Task 4: Modify Element in a List
+Modify an element at a specific index with a new value. 
+Print the updated list.
+"""
+
+list_mine[2] = "little baby"#modified 2 into little baby
+
+print(list_mine)
+
+"""
+Task 5: Append Multiple Elements to a List
+Append multiple elements to the end of the list. Print 
+the updated list.
+"""
+
+
+list_mine.append(["daughter", "big boy"])#listed appends
+
+print(list_mine)
+
+
+"""
+Task 6: Delete Element at a Specific Index
+Delete an element at a specific index. Print the updated 
+list.
+"""
+
+del list_mine[5]
+
+print(list_mine)
+
+"""
+Task 7: Slicing lists
+Create a new variable equal to the first 2 items of your list
+Then print out the new variable
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
