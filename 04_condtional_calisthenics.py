9808171c38116808389be1e9607cc9b1642a7fad
Jeriah Oliverasfair <joliverasfair@brooklynsteamcenter.org>
2025-09-16 10:51:43 -0400

    pp

---
+++ 04_condtional_calisthenics.py
@@ -0,0 +1,30 @@
+'''
+Exercise 1:
+Check if an integer is even and greater than 10.
+Return True if both conditions are met, False otherwise.
+'''
+if 5 > 10:
+    print(True)
+else:
+    print(False)
+
+
+'''
+Exercise 2:
+Determine the ticket price based on age and student status.
+Price is $10 if under 18 or a student, $20 otherwise.
+'''
+
+price = input("please put your age" )(18 and 20) # made price20 a ting variable
+
+if price == 18:        # made an if statement to determine what will be paid.
+    print("pay 10 dollars you ")
+elif price == 20:
+    print("pay 20 dollars your an adult")
+
+'''
+Exercise 3:
+Prompt the user to enter a fruit name. Check if the fruit is in the list. 
+If it is, print "Yes, that fruit is in the list." 
+If it's not, print "No, that fruit is not in the list."
+'''
