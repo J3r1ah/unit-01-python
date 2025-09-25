212ef6ebbe32d97685c23181e543469b36d60945
Jeriah Oliverasfair <joliverasfair@brooklynsteamcenter.org>
2025-09-25 10:50:59 -0400

    pp

---
+++ 07_rootin_ranges.py
@@ -0,0 +1,21 @@
+"""
+Exercise 1:
+Write a program to print numbers from 1 to 10 using a for loop.
+"""
+
+
+for x in range(1,11):
+    print(x)
+
+
+
+"""
+Exercise 2:
+Write a program to count by 10s from 900 to 1000
+"""
+
+
+for x in range(900,1000, 10):
+    print(x)
+
+
