672e6b9e1ef3faa1722b97fd33a5c37d7fca2c2c
Jeriah Oliverasfair <joliverasfair@brooklynsteamcenter.org>
2025-09-25 10:41:56 -0400

    pp

---
+++ 07_flipping_fors.py
@@ -0,0 +1,47 @@
+"""
+Exercise 1:
+Write a program to print each character of a given string using a for loop.
+"""
+print()
+print()
+
+
+program = ["git gud", "adino", "shaa"]
+
+for x in program:
+    print(x)
+
+print()
+print()
+
+"""
+Exercise 2:
+Write a program to calculate the sum of elements in a list using a for loop.
+"""
+print()
+print()
+
+
+mommy = [1,2,3,4]
+
+for num in mommy:
+    print(num)
+
+print()
+print()
+
+"""
+Exercise 3:
+Write a program to print the length of each word in a sentence using a for loop and a list.
+"""
+print()
+print()
+
+
+daddy = list("daddy, and, baby, is, lost")
+
+for x in daddy:
+    print(x)
+
+print()
+print()
