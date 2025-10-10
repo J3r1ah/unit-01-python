4b8b81109b5577059fb96dc1d9227ad08806ef96
Jeriah Oliverasfair <joliverasfair@brooklynsteamcenter.org>
2025-10-10 09:53:47 -0400

    pp

---
+++ 10_random.py
@@ -0,0 +1,35 @@
+import random
+
+
+"""
+Task 1 (random module):
+Write a program that simulates rolling a six-sided die 10 times.
+Print each roll result.
+"""
+
+
+for roll in range(1, 10):
+    result_of_dice = random.randint(1,6)
+    print("roll", roll + 1 , result_of_dice)
+
+
+"""
+Task 2 (random module):
+Write a program that generates 5 random floating-point numbers between 0 and 1.
+Then generate 5 random floating-point numbers between 10 and 20.
+Print both sets of numbers.
+"""
+
+for number in range(5):
+    random_float = random.randint(10,20)
+    print("float", number + 1.1 , random_float)
+
+
+for number in range
+
+
+    
+    
+   
+    
+
